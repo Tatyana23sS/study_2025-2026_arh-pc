9c7f8f6f05f3ef3e5862c2571c01a0fa239d16cd
Татьяна Семенченко <1032253509@pfur.ru>
2025-10-25 20:49:16 +0300

    lab4.asm

--- labs/lab04/lab4.asm
+++ labs/lab04/lab4.asm
@@ -1,16 +1,16 @@
 SECTION .data
 	hello:      DB "Semenchenko Tatyana",10 
-		helloLen:   equ $ - hello
+		helloLen:   EQU $ - hello
 SECTION .text
-	global _start           
+	GLOBAL _start           
 
 _start:                 
         mov eax, 4      
         mov ebx, 1    
         mov ecx, hello
         mov edx, helloLen
-        int 0x80        
+        int h80        
 	
 	mov eax, 1       
         mov ebx, 0      
-        int 0x80        
+        int h80        
